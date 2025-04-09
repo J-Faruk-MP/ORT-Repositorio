ecdb613fec96818a49b393ec63ac461b92f2beb6
Faruk <merrysfaruk162@gmail.com>
2025-04-09 15:24:20 -0300

    se modifico practica1

--- practica.java
+++ practica.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class practica {
-
 	public static void main(String[] args) {
         System.out.println("hola mundo");
-        System.out.println("cual es tu nombre");
+        Scanner input = new Scanner(System.in);
+        System.out.println("ingrese tu apellido");
+        String apellido = input.nextLine();
+        String apellido2 = "suarez";
+        String apelldio3= "lucciano";
+        System.out.println(apellido);
 
 
 
     }
+    
 }
