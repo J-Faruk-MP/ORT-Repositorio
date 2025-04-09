c1e1149f5520dd44685d6f282fc595fd48290ce8
Faruk <merrysfaruk162@gmail.com>
2025-04-09 14:57:42 -0300

    secondcode

---
+++ practica2.java
@@ -0,0 +1,10 @@
+public class practica2 {
+	public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("cual es tu nombre");
+
+
+
+    }
+    
+}
