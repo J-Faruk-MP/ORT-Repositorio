d234f1e4ac242a9d940ab75806a465a82521e600
Faruk <merrysfaruk162@gmail.com>
2025-04-09 15:30:55 -0300

    mmm

--- practica.java
+++ practica.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
-
+// esto es el primer codigo
 public class practica {
 
 	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("hola mundo");
         System.out.println("cual es tu nombre");
+        String nombre = input.nextLine();
+        System.out.println("cual es tu apellido");
+        String apellido = input.nextLine();
 
 
 
