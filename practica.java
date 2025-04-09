06cadf9785ab06b252120d62cb3361c5fdc5c7a4
Faruk <merrysfaruk162@gmail.com>
2025-04-09 14:54:30 -0300

    firstcode

---
+++ practica.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class practica {
+
+	public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("cual es tu nombre");
+
+
+
+    }
+}
