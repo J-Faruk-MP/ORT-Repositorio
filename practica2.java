c87b2922d126f42e438013612c6a65287d4aa877
Faruk <merrysfaruk162@gmail.com>
2025-04-09 15:02:55 -0300

    se agrego un input

--- practica2.java
+++ practica2.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class practica2 {
 	public static void main(String[] args) {
         System.out.println("hola mundo");
+        Scanner input = new Scanner(System.in);
         System.out.println("cual es tu nombre");
+        String nombre = input.nextLine();
 
 
 
